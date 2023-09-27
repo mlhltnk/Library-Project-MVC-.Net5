4de1795c431fc05c767d3de06dbb6128bb269e68
Melih ALtunok <altunokmelih@gmail.com>
2023-09-27 08:13:11 +0300

    category

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -3,15 +3,58 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
 
 namespace MvcKutuphane.Controllers
 {
     public class KategoriController : Controller
     {
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
+
+
         // GET: Kategori
         public ActionResult Index()
         {
+            var degerler=db.TBLKATEGORI.ToList();
+            return View(degerler);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriEkle(TBLKATEGORI p)
+        {
+            db.TBLKATEGORI.Add(p);
+            db.SaveChanges();
             return View();
         }
+
+        [HttpGet]
+        public ActionResult KategoriEkle()
+        { return View(); 
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKATEGORI.Find(id);
+            db.TBLKATEGORI.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var ktg = db.TBLKATEGORI.Find(id);
+
+            return View(ktg);
+        }
+    
+        public ActionResult KategoriGuncelle(TBLKATEGORI p)
+        {
+            var ktg = db.TBLKATEGORI.Find(p.ID);
+            ktg.AD=p.AD;   //kategorinin yeni adı=indexten yeni giriş yapılan ad
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+       
     }
 }
