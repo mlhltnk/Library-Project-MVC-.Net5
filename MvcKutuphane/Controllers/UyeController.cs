8bc4bf59b571bb82fb7481c12a85f742c4ca6395
mlhltnk <109691158+mlhltnk@users.noreply.github.com>
2023-09-29 21:53:48 +0300

    uyeson

--- MvcKutuphane/Controllers/UyeController.cs
+++ MvcKutuphane/Controllers/UyeController.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MvcKutuphane.Models.Entity;
+using PagedList;
+using PagedList.Mvc;
 
 
 namespace MvcKutuphane.Controllers
@@ -14,10 +16,63 @@ namespace MvcKutuphane.Controllers
         DBKUTUPHANEEntities4 db = new DBKUTUPHANEEntities4();
 
         // GET: Uye
-        public ActionResult Index()
+        public ActionResult Index(int sayfa=1)
         {
-            var degerler = db.TBLUYELER.ToList();
+            //var degerler = db.TBLUYELER.ToList();
+
+            var degerler = db.TBLUYELER.ToList().ToPagedList(sayfa, 3);   
+            //sayfa numarası sayfa değerinden başlasın, her sayfada 3 değer göstersin
+
             return View(degerler);
         }
+
+        public ActionResult UyeEkle(TBLUYELER p)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("UyeEkle");
+                //arka taraftaki dataannotation sağlanamadıysa personelekleye geri dön
+            }
+            db.TBLUYELER.Add(p);
+            db.SaveChanges();
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult UyeEkle()
+        {
+            return View();
+        }
+
+        public ActionResult UyeSil(int id)
+        {
+            var uye = db.TBLUYELER.Find(id);
+            db.TBLUYELER.Remove(uye);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult UyeGetir(int id)
+        {
+            var uye = db.TBLUYELER.Find(id);
+
+            return View(uye);
+        }
+
+        public ActionResult UyeGuncelle(TBLUYELER p)
+        {
+            var uye = db.TBLUYELER.Find(p.ID);
+            uye.AD = p.AD;   //kategorinin yeni adı=indexten yeni giriş yapılan ad
+            uye.SOYAD = p.SOYAD;
+            uye.MAIL= p.MAIL;
+            uye.KULLANICIADI= p.KULLANICIADI;
+            uye.SIFRE= p.SIFRE;
+            uye.OKUL= p.OKUL;
+            uye.TELEFON= p.TELEFON;
+            uye.FOTOGRAF= p.FOTOGRAF;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
