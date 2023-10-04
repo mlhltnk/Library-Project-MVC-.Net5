d4fe939eb1ed8a6e649fb8d1eb7cd5a28b323f8e
mlhltnk <109691158+mlhltnk@users.noreply.github.com>
2023-10-04 15:57:37 +0300

    ıslem

---
+++ MvcKutuphane/Controllers/IslemController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+
+namespace MvcKutuphane.Controllers
+{
+    public class IslemController : Controller
+    {
+        DBKUTUPHANEEntities4 db = new DBKUTUPHANEEntities4();
+        // GET: Islem
+        public ActionResult Index()
+        {
+            var degerler = db.TBLHAREKET.Where(x => x.ISLEMDURUM == true).ToList();
+            return View(degerler);
+        }
+    }
+}
--- MvcKutuphane/Controllers/OduncController.cs
+++ MvcKutuphane/Controllers/OduncController.cs
@@ -14,7 +14,7 @@ namespace MvcKutuphane.Controllers
         // GET: Odunc
         public ActionResult Index()
         {
-            var degerler = db.TBLHAREKET.ToList();
+            var degerler = db.TBLHAREKET.Where(x=>x.ISLEMDURUM == false).ToList();
             return View(degerler);
         }
 
@@ -30,6 +30,22 @@ namespace MvcKutuphane.Controllers
             db.TBLHAREKET.Add(p);
             db.SaveChanges();
             return View();
+        }
+
+        public ActionResult Odunciade(int id)
+        {
+            var odnc = db.TBLHAREKET.Find(id);
+
+            return View("Odunciade",odnc);
+        }
+
+        public ActionResult OduncGuncelle(TBLHAREKET p)
+        {
+            var hrk = db.TBLHAREKET.Find(p.ID);
+            hrk.UYEGETİRTARİH = p.UYEGETİRTARİH;   //kategorinin yeni adı=indexten yeni giriş yapılan ad
+            hrk.ISLEMDURUM = true;  //geri alınca true'ya dönsün
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
 
     }
