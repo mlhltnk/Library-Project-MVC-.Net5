2951c0eabd618cef254e0d261ea42a34a83b7c7c
mlhltnk <109691158+mlhltnk@users.noreply.github.com>
2023-10-07 16:48:16 +0300

    7.10.23

--- MvcKutuphane/Controllers/VitrinController.cs
+++ MvcKutuphane/Controllers/VitrinController.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcKutuphane.Models.Entity;
+using MvcKutuphane.Models.Siniflarim;
 
 namespace MvcKutuphane.Controllers
 {
     public class VitrinController : Controller
     {
+        DBKUTUPHANEEntities4 db = new DBKUTUPHANEEntities4();
         // GET: Vitrin
+
+        [HttpGet]
         public ActionResult Index()
         {
-            return View();
+            Class1 cs = new Class1();
+            cs.Deger1 = db.TBLKITAP.ToList();
+            cs.Deger2 = db.TBLHAKKIMIZDA.ToList();
+
+            //var degerler = db.TBLKITAP.ToList();
+
+            return View(cs);
+        }
+
+        [HttpPost]
+        public ActionResult Index(TBLILETISIM t) 
+        {
+            db.TBLILETISIM.Add(t);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
         }
     }
 }
